ed8cfc40d61afd22a013c1c6f3cd3e4bb4d7e289
Mikhail Kashkin <m@askpapir.com>
2018-04-26 23:35:56 +0300

    basic helpers

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var eslint = require('gulp-eslint');
+
+// Exclude node_modules
+var self = '!node_modules/**/*';
+
+gulp.task('eslint', (done) => {
+    gulp.src(['./*.js', self])
+        .pipe(debug({ title: 'eslint:' }))
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+
+    done();
+});
+
+// Tasks
+gulp.task('lint', gulp.series('eslint', (done) => {
+    done();
+}));
